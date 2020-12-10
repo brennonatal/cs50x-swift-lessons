506c64f232e322b4c3dce19d4efa3785825ecf6a
Brenno Natal <natalbrenno@gmail.com>
2020-12-10 15:16:36 -0300

    Initial pokedex

---
+++ Pokedex/Pokedex/PokemonViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class PokemonViewController: UIViewController {
+    var url: String!
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var type1Label: UILabel!
+    @IBOutlet var type2Label: UILabel!
+
+    func capitalize(text: String) -> String {
+        return text.prefix(1).uppercased() + text.dropFirst()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        nameLabel.text = ""
+        numberLabel.text = ""
+        type1Label.text = ""
+        type2Label.text = ""
+
+        loadPokemon()
+    }
+
+    func loadPokemon() {
+        URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+
+            do {
+                let result = try JSONDecoder().decode(PokemonResult.self, from: data)
+                DispatchQueue.main.async {
+                    self.navigationItem.title = self.capitalize(text: result.name)
+                    self.nameLabel.text = self.capitalize(text: result.name)
+                    self.numberLabel.text = String(format: "#%03d", result.id)
+
+                    for typeEntry in result.types {
+                        if typeEntry.slot == 1 {
+                            self.type1Label.text = typeEntry.type.name
+                        }
+                        else if typeEntry.slot == 2 {
+                            self.type2Label.text = typeEntry.type.name
+                        }
+                    }
+                }
+            }
+            catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+}
